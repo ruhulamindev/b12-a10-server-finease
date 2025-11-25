b6811839cbb7873d1c2a8ff77eaf901d15ebafeb
Ruhul Amin <ruhulamin824854@gmail.com>
2025-11-25 21:41:46 +0600

    Details Page Data Complete

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = 5000;
 app.use(cors());
 app.use(express.json());
@@ -26,22 +26,43 @@ async function run() {
     const db = client.db("b12-a10-server-finease");
     const fineaseCollection = db.collection("finance-all");
 
+
+
+
+    // get request
     app.get("/finance-all", async (req, res) => {
       const result = await fineaseCollection.find().toArray();
       // console.log(result)
       res.send(result);
     });
 
+
+
+
+    // post request
     app.post("/finance-all", async (req, res) => {
       const data = req.body;
-      console.log(data)
-      const result = await fineaseCollection.insertOne(data)
+      // console.log(data)
+      const result = await fineaseCollection.insertOne(data);
       res.send({
         success: true,
-        result
+        result,
       });
     });
 
+
+
+
+    // get request and detail page api create
+    app.get("/finance-all/:id", async (req, res) => {
+      const { id } = req.params;
+      // console.log(id)
+      const result = await fineaseCollection.findOne({ _id: new ObjectId(id) });
+
+    res.send({ success: true, result });
+    });
+
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
