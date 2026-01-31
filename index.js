e135c27c144ee1402927dd7425e61986dc286574
Ruhul Amin <ruhulamin824854@gmail.com>
2026-01-31 12:29:01 +0600

    cors problem solved

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const admin = require("firebase-admin");
 require("dotenv").config();
 const serviceAccount = require("./service.json");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 // Middlewares
 app.use(
